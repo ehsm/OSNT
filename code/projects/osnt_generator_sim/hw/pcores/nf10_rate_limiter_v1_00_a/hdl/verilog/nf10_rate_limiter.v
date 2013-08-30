48b648ece797458280da4adfd196f74b72ae6233
Muhammad Shahbaz <Muhammad.Shahbaz@gatech.edu>
2013-08-30 01:18:12 -0400

    updated nf10_rate_limiter

--- code/projects/osnt_generator_sim/hw/pcores/nf10_rate_limiter_v1_00_a/hdl/verilog/nf10_rate_limiter.v
+++ code/projects/osnt_generator_sim/hw/pcores/nf10_rate_limiter_v1_00_a/hdl/verilog/nf10_rate_limiter.v
@@ -145,7 +145,7 @@ module nf10_rate_limiter
     .s_axi_awready   (s_axi_awready),
 
     .rw_regs         (rw_regs),
-	.rw_defaults     ({32'd0,32'b10}),
+	.rw_defaults     (/*{NUM_RW_REGS*C_S_AXI_DATA_WIDTH{1'b0}}*/{32'b0, 32'b10}),
 	.wo_regs         (),
 	.wo_defaults     ({NUM_WO_REGS*C_S_AXI_DATA_WIDTH{1'b0}}),
 	.ro_regs         ()
